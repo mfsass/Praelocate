71895b6bfc4c29bbcba3a9d1b37c1ee58e0183cc
Jacques Le Roux <23068574@sun.ac.za>
2022-08-17 17:17:39 +0200

    Replace App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,6 @@ import "./App.css";
 import home from "./home-alt.svg";
 import React from 'react'
 import Locations from "./components/Locations";
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
-
-const containerStyle = {
-  width: '400px',
-  height: '400px'
-};
-
-const center = {
-  lat: -3.745,
-  lng: -38.523
-};
 
 function App() {
   return (
@@ -25,15 +14,6 @@ function App() {
       </header>
       <body>
         <img src={home} className="App-logo" alt="logo" />
-        <LoadScript googleMapsApiKey="AIzaSyAw71uaQ28Y-SABJAkLueUlhtdcN1JAPzI">
-          <GoogleMap
-          mapContainerStyle={containerStyle}
-          center={center}
-          zoom={10}
-          >
-          <></>
-          </GoogleMap>
-        </LoadScript>
         <Locations></Locations>
       </body>
     </div>
