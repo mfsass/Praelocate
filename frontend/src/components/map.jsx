a4c0851957e3394462e13de375e44a5f57c315dd
Jacques Le Roux <23068574@sun.ac.za>
2022-08-18 16:50:35 +0200

    Replace map.jsx

--- frontend/src/components/map.jsx
+++ frontend/src/components/map.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, MarkerF } from '@react-google-maps/api';
 import './map.css';
 
 const containerStyle = {
@@ -11,7 +11,26 @@ const center = {
     lat: -33.9328,
     lng: 18.8644
 };
-  
+
+const position1 = {
+    lat: -34.067290,
+    lng: 18.858110
+}
+
+const position2 = {
+    lat: -33.931151,
+    lng: 18.871074
+}
+
+const position3 = {
+    lat: -33.945824, 
+    lng: 18.858989
+}
+
+const midpoint = {
+    lat: -33.98142166666667, 
+    lng: 18.862724333333336
+}
 
 let Map = () => (
     <div className="map">
@@ -20,9 +39,21 @@ let Map = () => (
             <GoogleMap
             mapContainerStyle={containerStyle}
             center={center}
-            zoom={10}
+            zoom={10.5}
             >
-            </GoogleMap>
+            <MarkerF
+            position  = {position1}
+            icon = {"http://maps.google.com/mapfiles/ms/icons/red-dot.png"}/>
+            <MarkerF
+            position  = {position2}
+            icon = {"http://maps.google.com/mapfiles/ms/icons/red-dot.png"}/>
+            <MarkerF
+            position  = {position3}
+            icon = {"http://maps.google.com/mapfiles/ms/icons/red-dot.png"}/>        
+            <MarkerF
+            position  = {midpoint}
+            icon = {"http://maps.google.com/mapfiles/ms/icons/blue-dot.png"}/>  
+            </GoogleMap> 
         </LoadScript>
         </div>
     </div>
