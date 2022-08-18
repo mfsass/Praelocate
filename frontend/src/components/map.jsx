c08bc7815a52af117d098aa7fb6262a9741677ec
Jacques Le Roux <23068574@sun.ac.za>
2022-08-18 12:25:11 +0200

    Upload New File

---
+++ frontend/src/components/map.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import './map.css'
+
+const containerStyle = {
+    width: '400px',
+    height: '400px'
+  };
+  
+  const center = {
+    lat: -3.745,
+    lng: -38.523
+  };
+  
+
+const Map = () => (
+    <div className="map">
+        <div className="google-map">
+        <LoadScript googleMapsApiKey="AIzaSyAw71uaQ28Y-SABJAkLueUlhtdcN1JAPzI">
+          <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={10}
+          >
+          </GoogleMap>
+      </LoadScript>
+
+        </div>
+    </div>
+)
+
+export default Map
