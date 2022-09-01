739dfde7ac867c7cd57e730e5ef262630b1d14e1
Wannenburg, WP, Mr [23560762@sun.ac.za] <23560762@sun.ac.za>
2022-09-01 18:45:58 +0200

    Deleted Locations, made components, refactored much code

--- frontend/src/components/LocationBox.jsx
+++ frontend/src/components/LocationBox.jsx
@@ -2,35 +2,22 @@ import { React } from "react";
 import { StandaloneSearchBox } from "@react-google-maps/api";
 
 import "./locationBox.css";
+import { forwardRef } from "react";
 
-function LocationBox(props) {
-  // const [location1, setLocation1] = useState({
-  //   latitude: "",
-  //   longitude: "",
-  // });
-
-  // const handleChange = (event) => {
-  //   event.target.style["background-color"] = "green";
-  //   console.log("passing to parent");
-  //   props.passLocation(location1);
-  // };
-
+const LocationBox = forwardRef((props, ref) => {
   return (
     <div className="box">
       <label>{props.label}</label>
       <StandaloneSearchBox>
         <input
-          ref={props.locationStr}
+          ref={ref}
           type="text"
           placeholder={props.placeholder}
           style={props.inputStyle}
         ></input>
       </StandaloneSearchBox>
-      <button className="box button" type="button" onClick={props.handleChange}>
-        Apply
-      </button>
     </div>
   );
-}
+});
 
 export default LocationBox;
--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,14 +1,9 @@
-import React, { useState, useRef, useReducer } from "react";
+import React, { useState, useRef } from "react";
 import Geocode from "react-geocode";
-import {
-  GoogleMap,
-  LoadScript,
-  StandaloneSearchBox,
-  MarkerF,
-} from "@react-google-maps/api";
-import { useEffect } from "react";
-import "./map.css";
+import { GoogleMap, LoadScript, MarkerF } from "@react-google-maps/api";
+
 import LocationBox from "./LocationBox";
+import "./map.css";
 
 const libraries = ["places"];
 
@@ -39,115 +34,86 @@ const API_KEY = "AIzaSyAw71uaQ28Y-SABJAkLueUlhtdcN1JAPzI";
 Geocode.setApiKey(API_KEY);
 Geocode.setRegion("za");
 
-// const formReducer = (state, event) => {
-//   return {
-//     ...state,
-//     [event.name]: event.value,
-//   };
-// };
-
-let coordinates = [];
-
 function Map() {
-  // const [formData, setFormData] = useReducer(formReducer, {});
   const location1Str = useRef();
   const location2Str = useRef();
   const location3Str = useRef();
 
-  const [data, setData] = useState([]);
-
-  const [location1, setLocation1] = useState("");
-  const [location2, setLocation2] = useState("");
-  const [location3, setLocation3] = useState("");
-  const [midPoint, setMidPoint] = useState("");
+  const [location1, setLocation1] = useState(null);
+  const [location2, setLocation2] = useState(null);
+  const [location3, setLocation3] = useState(null);
   const [submitting, setSubmitting] = useState(false);
   const [shouldShowLocations, setShouldShowLocations] = useState(false);
   const [shouldShowMidPoint, setShouldShowMidPoint] = useState(false);
   const [allCoordinates, setAllCoordinates] = useState([]);
 
   const toggleShow = (event) => {
-    if (allCoordinates.length === 3) {
-      setAllCoordinates((allCoordinates) => [...allCoordinates, midPoint]);
-      setShouldShowLocations(true);
-      event.target.style["background-color"] = "#944040";
-    }
-    if (allCoordinates.length === 4) {
-      setShouldShowMidPoint(true);
-      event.target.style["background-color"] = "#2b65b1";
+    if (allCoordinates.midpoint) {
+      setShouldShowMidPoint((shouldShowMidPoint) => !shouldShowMidPoint);
+      changeColor(event, "#2b65b1");
     }
   };
 
-  useEffect(() => {
-    console.log(shouldShowLocations, shouldShowMidPoint);
-  }, [shouldShowLocations, shouldShowMidPoint]);
-
-  const changeColor = (event) => {
-    event.target.style["background-color"] = "#4cb98c";
+  const changeColor = (event, color) => {
+    let tempColor = color ? color : "#4cb98c";
+    event.target.style["background-color"] = tempColor;
   };
 
-  const handleSave = (e) => {
-    e.preventDefault();
+  const handleSave = (event) => {
+    event.preventDefault();
     if (
       !location1Str.current.value ||
       !location2Str.current.value ||
       !location3Str.current.value
     ) {
-      e.target.style["background-color"] = "red";
+      changeColor("red");
       return;
     }
-    setSubmitting(true);
 
-    if (location1Str.current.value === "") return;
-    Geocode.fromAddress(location1Str.current.value).then(
+    const locations = [
+      {
+        string: location1Str,
+        function: setLocation1,
+      },
+      {
+        string: location2Str,
+        function: setLocation2,
+      },
+      {
+        string: location3Str,
+        function: setLocation3,
+      },
+    ];
+    locations.map((entry) => {
+      return getGeoFromText(entry.string.current.value, entry.function);
+    });
+
+    changeColor(event);
+  };
+
+  const getGeoFromText = (text, changeLocation) => {
+    if (!text) {
+      changeColor("red");
+    }
+
+    Geocode.fromAddress(text).then(
       (response) => {
-        const responseCoords = response.results[0].geometry.location;
-        console.log(responseCoords);
-        setLocation1(() => responseCoords);
-        console.log(location2);
+        changeLocation(() => response.results[0].geometry.location);
       },
       (error) => {
         console.error(error);
       }
     );
-
-    if (location2Str === "") return;
-    Geocode.fromAddress(location2Str.current.value).then(
-      (response) => {
-        const responseCoords = response.results[0].geometry.location;
-        console.log(responseCoords);
-        setLocation2(() => responseCoords);
-        console.log(location2);
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
-
-    if (location3Str === "") return;
-    Geocode.fromAddress(location3Str.current.value).then(
-      (response) => {
-        const responseCoords = response.results[0].geometry.location;
-        console.log(responseCoords);
-        setLocation3(() => responseCoords);
-        console.log(location3);
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
-    changeColor(e);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setSubmitting(true);
     let timeOut = !location1 || !location2 || !location3 ? 3000 : 0;
     timeOut > 0
       ? console.log("Timing out")
       : console.log("No need for timeout");
     setTimeout(() => {
-      console.log(`loc1: ${location1}`);
-      console.log(`loc2: ${location2}`);
-      console.log(`loc3: ${location3}`);
       const requestOpt = {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -165,33 +131,11 @@ function Map() {
       (async () => {
         let info = await fetchFunc();
         console.log(info);
+        setAllCoordinates(info.allCoordinates);
+        setSubmitting(false);
       })();
-      setSubmitting(false);
+      setShouldShowLocations(true);
     }, timeOut);
-  };
-
-  const getPoint = (e) => {
-    e.preventDefault();
-    const requestOpt = {
-      method: "GET",
-      headers: { "Content-Type": "application/json" },
-    };
-    async function fetchFunc() {
-      return await fetch("/getLocations", requestOpt)
-        .then((response) => response.json())
-        .catch((error) => console.log(error));
-    }
-    (async () => {
-      let info = await fetchFunc();
-      // handle(e, info[3], setMidPoint);
-      setMidPoint(info.coordinates);
-      setData(info);
-      coordinates = info.allCoordinates;
-
-      setAllCoordinates((allCoordinates) => [...allCoordinates, location1]);
-      setAllCoordinates((allCoordinates) => [...allCoordinates, location2]);
-      setAllCoordinates((allCoordinates) => [...allCoordinates, location3]);
-    })(setMidPoint(""));
   };
 
   return (
@@ -199,139 +143,62 @@ function Map() {
       <LoadScript googleMapsApiKey={API_KEY} libraries={libraries}>
         <div className="locations">
           <form className="locations form" onSubmit={handleSubmit}>
-            {/* <LocationBox
+            <LocationBox
+              label={"First location"}
+              inputStyle={inputStyle}
+              ref={location1Str}
+              placeholder={"e.g. 'Neelsie'"}
+            />
+
+            <LocationBox
               label={"Second location"}
               inputStyle={inputStyle}
-              locationStr={location1Str}
-              placeholder={"e.g. 'Neelsie'"}
-              handleChange={(e) => {
-                handleChange(e, location1Str, setLocation1, location1);
-              }}
-            /> */}
+              ref={location2Str}
+              placeholder={"e.g. 'Endler'"}
+            />
 
-            {submitting && (
-              <div>
-                Submitting:
-                <ul>
-                  <li>
-                    Location1:
-                    <br /> lat: {location1.lat} | lon: {location1.lon}
-                  </li>
-                  <li>
-                    Location2:
-                    <br /> lat: {location2.lat} | lon: {location2.lon}
-                  </li>
-                  <li>
-                    Location3:
-                    <br /> lat: {location3.lat} | lon: {location3.lon}
-                  </li>
-                </ul>
-              </div>
-            )}
+            <LocationBox
+              label={"Third location"}
+              inputStyle={inputStyle}
+              ref={location3Str}
+              placeholder={"e.g. 'Praelexis'"}
+            />
 
-            <div className="box">
-              <label>First location</label>
-              <StandaloneSearchBox>
-                <input
-                  // ref={location1Str}
-                  // name="location1Str"
-                  type="text"
-                  placeholder="eg. 'Neelsie'"
-                  style={inputStyle}
-                  ref={location1Str}
-                  // onChange={handleChange}
-                ></input>
-              </StandaloneSearchBox>
+            <div className="box button">
+              <button
+                className="locations button"
+                type="submit"
+                onClick={(event) => {
+                  handleSave(event);
+                }}
+              >
+                Save
+              </button>
+              <button
+                className="locations button"
+                type="submit"
+                onClick={changeColor}
+              >
+                Submit
+              </button>
+              <button
+                className="locations button"
+                type="button"
+                onClick={toggleShow}
+              >
+                Show point
+              </button>
             </div>
-            <div className="box">
-              <label>Second location</label>
-              <StandaloneSearchBox>
-                <input
-                  ref={location2Str}
-                  // name="location2Str"
-                  type="text"
-                  placeholder="eg. 'Endler'"
-                  style={inputStyle}
-                  // onChange={handleChange}
-                ></input>
-              </StandaloneSearchBox>
-            </div>
-            <div className="box">
-              <label>Third location</label>
-              <StandaloneSearchBox>
-                <input
-                  ref={location3Str}
-                  // name="location3Str"
-                  type="text"
-                  placeholder="eg. 'Praelexis'"
-                  style={inputStyle}
-                  // onChange={handleChange}
-                ></input>
-              </StandaloneSearchBox>
-            </div>
-            <button
-              className="locations button"
-              type="submit"
-              onClick={(event) => {
-                handleSave(event);
-              }}
-            >
-              Save
-            </button>
-            <button
-              className="locations button"
-              type="submit"
-              onClick={changeColor}
-            >
-              Submit
-            </button>
           </form>
+          {submitting && <span>Submitting ... </span>}
 
-          <form className="locations getPoint" onSubmit={getPoint}>
-            <button
-              className="locations button"
-              type="submit"
-              onClick={changeColor}
-            >
-              Get point
-            </button>
-          </form>
-          <div className="locations showPoint">
-            <button
-              className="locations button"
-              type="button"
-              onClick={toggleShow}
-            >
-              Show point
-            </button>
-          </div>
+          {allCoordinates.midpoint && (
+            <span>
+              Midpoint:
+              {allCoordinates.midpoint.lat},{allCoordinates.midpoint.lng}
+            </span>
+          )}
         </div>
-
-        {shouldShowLocations && (
-          <>
-            <h3>Midpoint = {midPoint}</h3>
-            <table id="myTable" className="table table-available">
-              <thead>
-                <tr>
-                  <th>Location:</th>
-                  <th>Distances from Midpoint (kms)</th>
-                  <th>Travel time from Midpoint (mins)</th>
-                </tr>
-              </thead>
-              <tbody>
-                {Object.keys(data).map((value, index) => {
-                  return (
-                    <tr>
-                      <td>{data.allCoordinates[index]} </td>
-                      <td>{data.allDistances[index]}</td>
-                      <td>{data.allTimes[index]}</td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </>
-        )}
 
         <div className="googleMap">
           <GoogleMap
@@ -357,10 +224,7 @@ function Map() {
             )}
             {shouldShowMidPoint && (
               <MarkerF
-                position={{
-                  lat: midPoint[0],
-                  lon: midPoint[1],
-                }}
+                position={allCoordinates.midpoint}
                 icon={"http://maps.google.com/mapfiles/ms/icons/blue-dot.png"}
               />
             )}
@@ -372,9 +236,3 @@ function Map() {
 }
 
 export default Map;
-
-// location1.latitude = -33.93087933494542; -> eendrag
-// location1.longitude = 18.872240042486016;
-// location2.latitude = -33.93253245739606; -> neelsie
-// location2.longitude = 18.86538266947165;
-// Location 3 = -33.940743, 18.854595 Gino's
