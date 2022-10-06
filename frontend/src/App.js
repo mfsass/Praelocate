8eeb770f8e93c1742bf95263ca5c8243f4004408
Wannenburg, WP, Mr [23560762@sun.ac.za] <23560762@sun.ac.za>
2022-10-06 13:12:40 +0200

    Made dynamic locations [many, many hours of debugging :( ]

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React, { useRef } from "react";
 import Map from "./components/Map";
 import "./App.css";
+import StateTest from "./components/StateTest";
+import StateTest2 from "./components/StateTest2";
 
 const location = {
   address: "1600 Amphitheatre Parkway, Mountain View, california.",
@@ -34,6 +36,10 @@ function App() {
           </a>
         </div>
       </div>
+
+      {/* <div className="stateTest">
+        <StateTest />
+      </div> */}
       <div id="body" className="body">
         <Map location={location} />
       </div>
